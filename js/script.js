6a4fa86da6c23428062e7a8620aefa1e99cccde8
adatg <ada.gniazdowska@gmail.com>
2018-11-09 13:26:11 -0500

    First

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+
+// //shows content in the Performance Page
+// $('a').on('click', function(){
+//    var target = $(this).attr('rel');
+//    $("#"+target).show().siblings("div").hide();
+// });
